fc6fd051db2b3e43d33a4dc4d33dc3b48fe254b1
WALTER EDUARDO RAPALO SMITH <werapalo@uth.hn>
2024-08-13 14:50:21 -0600

    Add files via upload

---
+++ EjerExamen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerexamen;
+
+/**
+ *
+ * @author rapal
+ */
+public class EjerExamen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
